73b3302585c4ea16e2cfc01c4ec0a3ae626cbf46
LucDubs <lucas.dubs5@gmail.com>
2021-09-20 17:07:25 -0300

    Up: projeto criado

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# NetFlix-clon
+# Clone-NetFlix
 Projeto de clonagem da Netflix, porém de um jeito mais pessoal, relacionado ao lado gamer, e com o nome de GamerFlix
+
+Preview:
+
+
+<img src="img/preview.jpg">
---
+++ js/owl/setup.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+// EVENTOS
+function clicou(){
+    alert("Os eventos da história de Mortal Kombat 11 começam logo após o final de Mortal Kombat X. Raiden bagunçou o tempo para impedir Shao Kahn e depois pegou o amuleto de Shinnok, corrompendo-se no processo e decidindo deixar a diplomacia de lado e usar a violência. Por ele ter alterado o equilíbrio das coisas, a deusa Kronika, guardiã do Tempo, decide pará-lo a qualquer custo, criando alianças com os espectros de Liu Kang e Kitana e com o Shao Kahn vivo no passado. Enquanto isso, um Raiden do passado se une a Kotal Kahn e às versões do presente e passado dos guerreiros de EarthRealm.")
+}
